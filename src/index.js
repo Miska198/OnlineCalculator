3562d1dfa47d2281ee49ad6fd49f9ac119e20577
Miska198 <43399759+Miska198@users.noreply.github.com>
2018-11-13 15:43:00 +0100

    hotovo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function solveExpression() {
+/*function solveExpression() {
   var priklad = document.getElementById("priklad").value;
   document.getElementById("vysledek").innerHTML = "Vysledek je: " + priklad;
 }
@@ -7,6 +7,61 @@ var input = document.getElementById("priklad");
 
 input.addEventListener("keyup", function(event) {
   event.preventDefault();
+  if (event.keyCode === 13) {
+    solveExpression();
+  }
+});
+*/
+function solveExpression() {
+  var vyraz = document.getElementById("vyraz").value;
+  let leva = 0;
+  let prava = 0;
+  var zmena = true;
+  var operator = "";
+  for (let i = 0; i < vyraz.length; i++) {
+    if (vyraz.charAt(i) == "+") {
+      operator = "+";
+      zmena = false;
+    } else if (vyraz.charAt(i) == "-") {
+      operator = "-";
+      zmena = false;
+    } else if (vyraz.charAt(i) == "*") {
+      operator = "*";
+      zmena = false;
+    } else if (vyraz.charAt(i) == "/") {
+      operator = "/";
+      zmena = false;
+    } else {
+      if (zmena) {
+        leva = leva * 10 + parseInt(vyraz.charAt(i));
+      } else {
+        prava = prava * 10 + parseInt(vyraz.charAt(i));
+      }
+    }
+  }
+  var output = 0;
+  if (operator === "+") {
+    output = leva + prava;
+  } else if (operator === "-") {
+    output = leva - prava;
+  } else if (operator === "*") {
+    output = leva * prava;
+  } else if (operator === "/") {
+    output = leva / prava;
+  } else {
+    output = NaN;
+  }
+  document.getElementById("vysledek").value =
+    leva + " " + operator + " " + prava + " = " + output;
+}
+
+var input = document.getElementById("vyraz");
+
+// Spustí funkci, když uživatel zmáčkne Enter
+input.addEventListener("keyup", function(event) {
+  // Zruší výchozí funkci klávesy, pokud nějaká existuje
+  event.preventDefault();
+  // 13 je hodnota klávesy Enter
   if (event.keyCode === 13) {
     solveExpression();
   }
